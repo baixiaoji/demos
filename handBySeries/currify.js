1e6a22a24775a272cee284741f3ad6d9212064a5
baixioaji <1041874421@qq.com>
2020-03-15 14:38:07 +0800

    feat: 添加了柯理化部分

---
+++ handBySeries/currify.js
@@ -0,0 +1,19 @@
+const currify = (
+    fn, 
+    arr = []
+) => (...args) => {
+    if (args.length + arr.length === fn.length) {
+        return fn(...args, ...arr);
+    } else {
+        return currify(fn, [...arr, ...args]);
+    }
+}
+
+
+// sprit by compostion softwave
+const currify2 = (
+    fn, 
+    arr = []
+) => (...args) => (
+    a => a.length === fn.length ? fn(...a) : currify2(fn, a)
+)([...arr, ...args])
