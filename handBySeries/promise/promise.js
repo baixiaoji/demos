2cbd1bc3de54a5f4b52030d5b1c1360115ab4c3c
baixioaji <1041874421@qq.com>
2020-03-16 15:53:44 +0800

    repeat promise

--- handBySeries/promise/promise.js
+++ handBySeries/promise/promise.js
@@ -1,99 +1,90 @@
-// 简易版实现  https://zhuanlan.zhihu.com/p/21834559
-export default class HD {
+class HD {
     static PENDING = 'pending';
     static RESOLVE = 'resolve';
     static REJECT = 'reject';
- constructor(executor) {
-     this.status = HD.PENDING;
-    this.value = '';
-    this.callbacks = [];
-    
-    executor(this.resolve.bind(this), this.reject.bind(this))
- }
- 
- resolve(value) {
-     if (this.status !== HD.PENDING) return;
-    this.status = HD.RESOLVE; 
-    this.value = value;
-     
-    this.callbacks.forEach((obj) => {
-        obj.onSuccess(value)
-    })
- }
+    constructor(executor) {
+        this.status = HD.PENDING;
+        this.value = '';
+        this.callbacks = [];
 
- reject(reason) {
-     if (this.status !== HD.PENDING) return;
-     this.status = HD.REJECT;
-    this.value = reason;
+        executor(this.resolve.bind(this), this.reject.bind(this));
+    }
+    resolve(value) {
+        if (this.status !== HD.PENDING) return;
+        this.value = value;
+        this.callbacks.forEach(callback => {
+            callback.onSuccess(value);
+        })
+    }
+    reject(reason){
+        if (this.status !== HD.PENDING) return;
+        this.value = reason;
+        this.callbacks.forEach(callback => {
+            callback.onError(reason);
+        })
+    }
 
-    this.callbacks.forEach((obj) => {
-        obj.onError(reason)
-    })
- }
-
- then(onSuccess, onError) {
-     if (typeof onSuccess !== 'function') {
-         onSuccess = (value) => value;
-     }
-     if (typeof onError !== 'function') {
-         onError = reason => {throw reason}
-     }
-    const promise2 =  new HD((resolve, reject) => {
-        if (this.status === HD.RESOLVE) {
-            setTimeout(() => {
-                try {
-                    this.parse(promise2, onSuccess(this.value), resolve, reject)    
-                } catch (error) {
-                    reject(error)
-                }
-                
-               
-            })
+    then(onSuccess, onError) {
+        if (typeof onSuccess !== 'function') {
+            onSuccess = (value) => value;
         }
-        if (this.status === HD.REJECT) {
-            setTimeout(() => {
-                try {
-                    this.parse(promise2, onError(this.value), resolve, reject)    
-                } catch (error) {
-                    reject(error);
-                }
-                
-            })
+        if (typeof onError !== 'function') {
+            onError = (reason) => {throw reason};
         }
-   
-        if (this.status === HD.PENDING) {
-            this.callbacks.push({
-                onSuccess: (value) => {
+        const hd2 = new HD((resolve, reject) => {
+            if (this.status === HD.RESOLVE) {
+                setTimeout(() => {
                     try {
-                        this.parse(promise2, onSuccess(value), resolve, reject)    
+                        this.parse(hd2, onSuccess(this.value), resolve, reject)
+                    } catch(error) {
+                        reject(error)
+                    }
+                    
+                });
+            }
+    
+            if (this.status === HD.REJECT) {
+                setTimeout(() => {
+                    try {
+                        this.parse(hd2, onError(this.value), resolve, reject);
                     } catch (error) {
                         reject(error);
                     }
-                },
-                onError: (reason) => {
-                    try {
-                        this.parse(promise2, onError(reason), resolve, reject)
-                    }catch(err) {
-                        reject(err);
-                    }
-                },
-            })
-        }
-    })
+                    
+                })
+            }
     
-    return promise2;
- }
-
- parse(promise, result, resolve, reject) {
-     if (promise === result) {
-         throw TypeError('Chainning Refer Error');
-     }
-    if (result instanceof HD) {
-        // result 依旧pending状态需要 递归处理
-        result.then(resolve, reject)
-    } else {
-        // A+ 中这边还有一些判断 2.3部分
-        resolve(result)
-    }     
- }
+            if (this.status === HD.PENDING) {
+                this.callbacks.push({
+                    onSuccess: (value) => {
+                        try {
+                            this.parse(hd2, onSuccess(value), resolve, reject);
+                        } catch (error) {
+                            reject(error);
+                        }
+                       
+                    },
+                    onError: (reason) => {
+                        try {
+                            this.parse(hd2, onError(reason), resolve, reject);
+                        } catch (error) {
+                            reject(error)
+                        }
+                        
+                    },
+                })
+            }
+        })
+        
+    }
+    parse(promise, result, resolve, reject) {
+        if (promise === result) {
+            return new TypeError('chainning referernce')
+        }
+        if (result instanceof HD) {
+            result.then(resolve, reject)
+        } else {
+            resolve(result)
+        }
+    }
 }
