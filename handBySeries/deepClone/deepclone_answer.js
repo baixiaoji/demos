4326b6bba96b127501149988269e09d86c5cf4e0
baixioaji <1041874421@qq.com>
2020-05-04 14:19:41 +0800

    feat: add some advance comments

--- handBySeries/deepClone/deepclone_answer.js
+++ handBySeries/deepClone/deepclone_answer.js
@@ -1,3 +1,8 @@
+// having some drawback
+// cannot copy the obj having to many nest property, which cause the stackoverflow
+// the solution see  https://juejin.im/post/5bc1ae9be51d450e8b140b0c
+// change recursion to loop and the implement wa difference and more complex
+// how to clear the cache you can change the deepClone to the class that you can using the instance which contain itself cache weakMap
 const cache = new WeakMap();
 export default function deepClone(target) {
   if (target instanceof Object) {
