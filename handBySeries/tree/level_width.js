7c7200846da592c882d773cac639ae641a1de262
baixioaji <1041874421@qq.com>
2020-05-23 13:41:57 +0800

    levelWidth level

---
+++ handBySeries/tree/level_width.js
@@ -0,0 +1,38 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+    // 增加一个结束mark标记当前层级   核心用到了 广度优先遍历
+    const arr = [root,'end'];
+    const count = [];
+    let index = 0
+    while(arr.length > 1) {
+        const first = arr.shift();
+
+        if (first === 'end') {
+            index += 1;
+            // 'end' tag push last arr
+            arr.push(first);
+            continue;
+        }
+        if (typeof count[index] === 'undefined') {
+            count[index] = 0;
+        }
+        count[index] += 1;
+        arr.push(...first.children);
+    }
+
+    return count;
+}
+
+module.exports = levelWidth;
