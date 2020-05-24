bffd4e83f496cb330a002bee43c7d59baad72f1e
baixioaji <1041874421@qq.com>
2020-05-24 08:07:23 +0800

    more method to implement reverse int

--- handBySeries/reverseint/reverseint.js
+++ handBySeries/reverseint/reverseint.js
@@ -30,4 +30,7 @@ function reverseInt(n) {
   return parseInt(numberString.split('').reverse().join('')) * numberSign;
 }
 
+function _reverseInt(n) {
+  return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+}
 module.exports = reverseInt;
