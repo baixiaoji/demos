41e4081c45d23e4bdda752aed30a668e219ba2f6
baixioaji <1041874421@qq.com>
2020-05-24 08:04:45 +0800

    implement palindrome

---
+++ handBySeries/palindrome/palindrome.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+  let reversed = str.split('').reduce((rev, char) => char + rev, '');
+  return str === reversed;
+}
+
+function _palidrome(str) {
+  return str.split('').every((char, index)=> {
+    return char === str[str.length - 1 - index];
+  })
+}
+
+module.exports = palindrome;
