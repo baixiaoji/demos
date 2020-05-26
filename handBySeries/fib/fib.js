e07fc2fa407df430f3972a5158b7d929b9d88c66
baixioaji <1041874421@qq.com>
2020-05-26 08:21:35 +0800

    fib implement

---
+++ handBySeries/fib/fib.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function memoization(fn) {
+  const cache = {}
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+
+    cache[args] = result;
+
+    return result
+  }
+}
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return  fib(n - 1) + fib( n - 2);
+}
+
+module.exports = memoization(fib);
