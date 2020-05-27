00b2fef83c351168f7f4a9932a521d32ddace9f7
baixioaji <1041874421@qq.com>
2020-05-27 09:08:10 +0800

    linkedlist practice

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     presets: [["@babel/preset-env", { "modules": 'commonjs' }]],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: ['@babel/plugin-proposal-class-properties', "@babel/plugin-transform-runtime"],
 }
