75e78bbabb9b7f10f5ecd75e93e85e995315d28b
baixioaji <1041874421@qq.com>
2020-05-29 08:50:09 +0800

    add leetcode 19

---
+++ handBySeries/leetcode/19-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  const dummyHead = {next: head};
+  let slow = dummyHead;
+  let fast = dummyHead;
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  slow.next = slow.next.next;
+
+  return dummyHead.next;
+};
