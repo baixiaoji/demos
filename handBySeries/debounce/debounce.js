f6204b437d55d689238655f0274165235923f273
baixioaji <1041874421@qq.com>
2020-05-29 08:41:41 +0800

    dir clear up

--- handBySeries/debounce/debounce.js
+++ handBySeries/debounce/debounce.js
@@ -1,9 +1,9 @@
 // debounce 防抖 一段时间之后才会执行，若期间发生同样事件，会重置计算时间
 // 明确函数体，知道入参是什么  和 输出是什么
 function debounce(fn, wait, immediate) {
-    const timer = null;
+    let timer = null;
     return function () {
-        const result = undefined;
+        let result = undefined;
 
         const args = arguments;
         if (timer) clearTimeout(timer);
